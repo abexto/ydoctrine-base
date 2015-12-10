26880d539f6aa66c2760d82390ba574b5d48fa2d
andreas <andreas@hc-devel-xu12>
2015-12-10 03:55:35 +0100

    zwico

---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+/* 
+ * Copyright 2015 Andreas Prucha, Abexto - Helicon Software Development.
+ */
+
---
+++ tests/unit/lib/doctrine/cache/YiiCacheTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Copyright 2015 Andreas Prucha, Abexto - Helicon Software Development.
+ */
+
+namespace abexto\ydc\base\tests\unit\lib\doctrine\cache;
+
+/**
+ * Description of YiiCache
+ *
+ * @author Andreas Prucha, Abexto - Helicon Software Development
+ */
+class YiiCache extends 
+{
+    //put your code here
+}
